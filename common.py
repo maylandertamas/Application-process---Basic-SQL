0045aea7495de311cc630c51df36fede0fa2ff65
maylandertamas <maylandertamas@gmail.com>
2017-05-09 19:35:57 +0200

    created menu structure and input verification

--- common.py
+++ common.py
@@ -1,3 +1,8 @@
+import manage_db
+import sys
+import os
+
+
 def print_table(table, title_list):
     border_horizontal = '_'
     border_vertical = '|'
@@ -13,3 +18,42 @@ def print_table(table, title_list):
     for row in table:
         print(separate_rows.format(*row))
         print(separate_crosses)
+
+
+def usr_input_verification(user_input):
+    try:
+        user_input = int(user_input)
+    except ValueError as error:
+        print("Please insert a correct input!")
+        print(error)
+    return user_input
+
+def menu_options():
+    user_input = input("Please choose a menu point! ")
+    input_verified = usr_input_verification(user_input)
+
+    if input_verified == 1:
+        print_table(manage_db.query_nicknames(), ["Nick names"])
+    elif input_verified == 2:
+        manage_db.query_nicknames()
+    elif input_verified == 3:
+        manage_db.query_for_carol()
+    elif input_verified == 4:
+        manage_db.query_for_other_girl()
+    elif input_verified == 5:
+        manage_db.insert_into_app_database()
+    elif input_verified == 6:
+        manage_db.update_phone_num()
+    elif input_verified == 7:
+        manage_db.delete_applicants()
+    elif user_input == 0:
+        sys.exit(0)
+    else:
+        raise KeyError("There is no such an option!")
+
+
+def print_menu():
+    print("Database Manager")
+    menu_points = ["Menup1", "Menup2", "Menup3", "Menup4", "Menup5", "Menup6", "Menup7", "Exit"]
+    for index, option in enumerate(menu_points):
+        print("[{0}] {1}".format(index+1, option))
