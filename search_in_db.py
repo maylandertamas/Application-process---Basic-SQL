3f4b2babc4a07ed0081037b03186fb1ee2e2cd68
maylandertamas <maylandertamas@gmail.com>
2017-05-10 11:22:45 +0200

    Started a bonus search module for db

--- search_in_db.py
+++ search_in_db.py
@@ -3,6 +3,7 @@ import manage_db
 
 cur = manage_db.cur
 
+
 def get_col_titles(database_name):
     database_name_str = str(database_name)
     cur.execute("Select * FROM " + database_name_str)
@@ -13,44 +14,45 @@ def get_col_titles(database_name):
 MENTOR_DB_COL_TITLES = get_col_titles("mentors")
 
 
-def mentor_search_menu_points():
-    user_input = input("Please choose a menu point! ")
-    input_verified = common.usr_input_verification(user_input)
-    if input_verified == 1:
-        search_in_db("first_name")
-    elif input_verified == 2:
-        search_in_db("last_name")
-    elif input_verified == 3:
-        search_in_db("nick_name")
-    elif input_verified == 4:
-        search_in_db("phone_number")
-    elif input_verified == 5:
-        search_in_db("email")
-    elif input_verified == 6:
-        search_in_db("city")
-    elif input_verified == 7:
-        search_in_db("favourite_number")
-    elif input_verified == 0:
-        sys.exit(0)
-    else:
-        raise KeyError("There is no such an option!")
+def mentor_search_menu():
+    while True:
+        common.print_menu("Search in mentors's database menu",
+                          ["Search by first name", "Search by last name", "Search by nick name",
+                           "Search by phone number", "Search by email adress", "Search by city",
+                           "Search by favourite number"])
+        user_input = input("Please choose a menu point! ")
+        input_verified = common.input_verification(user_input, "number")
+        if input_verified == 1:
+            search_in_mentors_db("first_name")
+        elif input_verified == 2:
+            search_in_mentors_db("last_name")
+        elif input_verified == 3:
+            search_in_mentors_db("nick_name")
+        elif input_verified == 4:
+            search_in_mentors_db("phone_number")
+        elif input_verified == 5:
+            search_in_mentors_db("email")
+        elif input_verified == 6:
+            search_in_mentors_db("city")
+        elif input_verified == 7:
+            search_in_mentors_db("favourite_number")
+        elif input_verified == 0:
+            manage_db.main()
+        else:
+            raise KeyError("There is no such an option!")
 
 
-def search_in_db(column):
+def search_in_mentors_db(column):
     if column == "favourite_number":
-        try:
-            user_input = int(input("Insert a number input to search by"))
-        except ValueError as err:
-            print("Please give me a real number!")
-            print(err)
+        user_input = input("Insert a number input to search by: ")
+        common.input_verification(user_input, "number")
+        cur.execute("SELECT * FROM mentors\
+                     WHERE " + column + " = " + str(user_input) + ";")
+        search_result = cur.fetchall()
     else:
-        try:
-            user_input = str(input("Insert a text input to search by"))
-        except ValueError as err:
-            print("Please give me a real number!")
-            print(err)
-    
-    cur.execute("SELECT * FROM mentors\
-                 WHERE " + column + " LIKE " + "\'" + user_input + "%\'")
-    search_result = cur.fetchall()
-    common.print_table(search_result, MENTOR_DB_COL_TITLES[1::])
+        user_input = input("Insert a text input to search by: ")
+        common.input_verification(user_input, "string")
+        cur.execute("SELECT * FROM mentors\
+                     WHERE lower(" + column + ") LIKE " + "\'%" + user_input + "%\'")
+        search_result = cur.fetchall()
+    common.print_table(search_result, MENTOR_DB_COL_TITLES)
