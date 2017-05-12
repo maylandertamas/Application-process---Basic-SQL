36f54025d86596f83cda1725fa8f3eeb33abd258
maylandertamas <maylandertamas@gmail.com>
2017-05-12 14:33:07 +0200

    added system clear for print table

--- common.py
+++ common.py
@@ -1,6 +1,7 @@
 import manage_db
 import sys
 import search_in_db
+import os
 
 
 def print_table(table, title_list):
@@ -11,7 +12,7 @@ def print_table(table, title_list):
     lengths = [max(map(len, map(str, col))) for col in columns]
     separate_rows = border_vertical + border_vertical.join(' {:^%d} ' % l for l in lengths) + border_vertical
     separate_crosses = border_cross + border_cross.join(border_horizontal * (l+2) for l in lengths) + border_cross
-
+    os.system('clear')
     print(separate_crosses)
     print(separate_rows.format(*title_list))
     print(separate_crosses)
